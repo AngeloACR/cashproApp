86dba407e0a69dd57ebd7fc88ba99c0f93d7dcdc
Jefferson Brioso <jabbsontouch@gmail.com>
2022-06-16 18:11:24 -0400

    response vista pagos

--- cashproApp/src/app/dashboard/components/dialog/dialog.component.ts
+++ cashproApp/src/app/dashboard/components/dialog/dialog.component.ts
@@ -7,9 +7,30 @@ import { Component, OnInit } from '@angular/core';
 })
 export class DialogComponent implements OnInit {
 
+  view: number = 3;
+  width: number = 0;
+  screen: boolean = false;
+
   constructor() { }
 
   ngOnInit(): void {
+  }
+
+  comprobarScreen(width:number) {
+    if(width > 768) {
+      this.screen = true;
+    } else {
+      this.screen = false;
+      this.view = 0;
+    }
+  }
+
+  siguiente(): void {
+    this.view ++;
+  }
+
+  volver(): void {
+    this.view --;
   }
 
 }
--- cashproApp/src/app/dashboard/pagos/containers/pasarela/pasarela.component.ts
+++ cashproApp/src/app/dashboard/pagos/containers/pasarela/pasarela.component.ts
@@ -6,10 +6,34 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./pasarela.component.scss']
 })
 export class PasarelaComponent implements OnInit {
+  
+  view: number = 2;
+  width: number = 0;
+  screen: boolean = false;
 
   constructor() { }
 
   ngOnInit(): void {
+    this.width = screen.width;
+    this.comprobarScreen(this.width);
   }
+
+  comprobarScreen(width:number) {
+    if(width > 768) {
+      this.screen = true;
+    } else {
+      this.screen = false;
+      this.view = 0;
+    }
+  }
+
+  siguiente(): void {
+    this.view ++;
+  }
+
+  volver(): void {
+    this.view --;
+  }
+
 
 }
